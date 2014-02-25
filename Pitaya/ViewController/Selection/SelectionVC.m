96feb0a7ae45b7da10e71ccc7179a5bf37605d6a
魏哲 <123nobody@gmail.com>
2014-02-25 15:38:45 +0800

    增加了精选页的分类筛选功能。

--- Pitaya/ViewController/Selection/SelectionVC.m
+++ Pitaya/ViewController/Selection/SelectionVC.m
@@ -10,11 +10,16 @@
 #import "SVPullToRefresh.h"
 #import "SelectionCell.h"
 
-@interface SelectionVC () <UICollectionViewDataSource, UICollectionViewDelegate>
+@interface SelectionVC () <UICollectionViewDataSource, UICollectionViewDelegate, UITableViewDataSource, UITableViewDelegate>
 
 @property (nonatomic, strong) NSMutableArray *selectionArray;
+@property (nonatomic, assign) NSInteger currentCategoryIndex;
 
 @property (nonatomic, strong) IBOutlet UICollectionView *collectionView;
+@property (nonatomic, strong) IBOutlet UIButton *categoryButton;
+@property (nonatomic, strong) UIPopoverController *popover;
+@property (nonatomic, strong) UITableView *popoverTableView;
+@property (nonatomic, strong) NSArray *categoryTitleArray;
 
 @end
 
@@ -26,7 +31,7 @@
 {
     __weak __typeof(&*self)weakSelf = self;
     
-    [GKDataManager getSelectionListWithTimestamp:[[NSDate date] timeIntervalSince1970] cateId:0 count:kRequestSize success:^(NSArray *dataArray) {
+    [GKDataManager getSelectionListWithTimestamp:[[NSDate date] timeIntervalSince1970] cateId:self.currentCategoryIndex count:kRequestSize success:^(NSArray *dataArray) {
         [weakSelf.collectionView.pullToRefreshView stopAnimating];
         weakSelf.collectionView.showsInfiniteScrolling = !(dataArray.count < kRequestSize);
         
@@ -46,7 +51,7 @@
     
     __weak __typeof(&*self)weakSelf = self;
     
-    [GKDataManager getSelectionListWithTimestamp:timestamp cateId:0 count:kRequestSize success:^(NSArray *dataArray) {
+    [GKDataManager getSelectionListWithTimestamp:timestamp cateId:self.currentCategoryIndex count:kRequestSize success:^(NSArray *dataArray) {
         [weakSelf.selectionArray addObjectsFromArray:dataArray];
         [weakSelf.collectionView reloadData];
         [weakSelf.collectionView.infiniteScrollingView stopAnimating];
@@ -62,6 +67,15 @@
 }
 
 #pragma mark - Selector Method
+
+- (IBAction)tapCategoryButton:(id)sender
+{
+    UIViewController *contentVC = [[UIViewController alloc] init];
+    [contentVC.view addSubview:self.popoverTableView];
+    _popover = [[UIPopoverController alloc] initWithContentViewController:contentVC];
+    self.popover.popoverContentSize = self.popoverTableView.frame.size;
+    [self.popover presentPopoverFromBarButtonItem:self.navigationItem.rightBarButtonItem permittedArrowDirections:UIPopoverArrowDirectionUp animated:YES];
+}
 
 #pragma mark - UICollectionViewDataSource
 
@@ -89,6 +103,61 @@
 
 #pragma mark - UICollectionViewDelegate
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 50.f;
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 12;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"PopoverCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    if (indexPath.row == self.currentCategoryIndex) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+    
+    cell.textLabel.text = self.categoryTitleArray[indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    [tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:self.currentCategoryIndex inSection:0]].accessoryType = UITableViewCellAccessoryNone;
+    self.currentCategoryIndex = indexPath.row;
+    [tableView reloadData];
+    
+    [self.categoryButton setTitle:self.categoryTitleArray[self.currentCategoryIndex] forState:UIControlStateNormal];
+    [self.popover dismissPopoverAnimated:YES];
+    [self.collectionView setContentOffset:CGPointZero animated:YES];
+    
+    double delayInSeconds = 0.5;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self.collectionView triggerPullToRefresh];
+    });
+}
+
 #pragma mark - Life Cycle
 
 - (id)initWithCoder:(NSCoder *)aDecoder
@@ -96,6 +165,7 @@
     self = [super initWithCoder:aDecoder];
     if (self) {
         _selectionArray = @[].mutableCopy;
+        _categoryTitleArray = @[@"全部", @"女装", @"男装", @"孩童", @"配饰", @"美容", @"科技", @"居家", @"户外", @"文化", @"美食", @"玩乐"];
     }
     return self;
 }
@@ -103,6 +173,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    _popoverTableView = [[UITableView alloc] initWithFrame:CGRectMake(0.f, 0.f, 200.f, 600.f) style:UITableViewStylePlain];
+    self.popoverTableView.dataSource = self;
+    self.popoverTableView.delegate = self;
+    self.popoverTableView.scrollEnabled = NO;
+    self.currentCategoryIndex = 0;
 }
 
 - (void)viewDidAppear:(BOOL)animated
