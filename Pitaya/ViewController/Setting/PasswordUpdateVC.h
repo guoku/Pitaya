aef3cc88527679730d9ef9cb5d84d2f44b4a74aa
魏哲 <123nobody@gmail.com>
2014-04-23 16:04:40 +0800

    增加了修改密码功能。

---
+++ Pitaya/ViewController/Setting/PasswordUpdateVC.h
@@ -0,0 +1,13 @@
+//
+//  PasswordUpdateVC.h
+//  Pitaya
+//
+//  Created by 魏哲 on 14-4-23.
+//  Copyright (c) 2014年 Guoku. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface PasswordUpdateVC : BaseViewController
+
+@end
